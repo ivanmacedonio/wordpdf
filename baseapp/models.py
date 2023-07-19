9968ab8e80b66439576f74680d34dbb0a214487a
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-07-19 18:43:45 -0300

    sizemodif

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
+
+def validar_tamano_pdf(value):
+    # Tamaño máximo permitido en bytes (por ejemplo, 5 MB)
+    max_size = 5 * 1024 * 1024
+
+    if value.size > max_size:
+        raise ValidationError("El tamaño del archivo no puede exceder 5 MB.")
 
 
 class WordHistory(models.Model):
@@ -10,7 +19,11 @@ class WordHistory(models.Model):
         return self.word
     
 class FileModel(models.Model):
-    filedata = models.FileField(upload_to='uploads/')                        
+    filedata = models.FileField(upload_to='uploads/',
+                                validators=[
+            FileExtensionValidator(allowed_extensions=['pdf']),
+            validar_tamano_pdf
+        ])                        
     line = models.IntegerField()
     word = models.CharField(max_length=128, null=False, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
